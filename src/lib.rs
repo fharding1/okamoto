6475a49c9d6660f9507c4a89f17baf315b424afe
Franklin Harding <fharding1@protonmail.com>
2024-11-10 11:38:58 -0500

    Fix bug, add dlrepr test

--- src/lib.rs
+++ src/lib.rs
@@ -4,71 +4,60 @@ mod okamoto;
 pub use crate::errors::*;
 pub use crate::okamoto::*;
 
-use curve25519_dalek::ristretto::RistrettoPoint;
-use curve25519_dalek::scalar::Scalar;
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
+    use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+    use curve25519_dalek::ristretto::RistrettoPoint;
+    use curve25519_dalek::scalar::Scalar;
+
     use super::*;
 
     #[test]
     fn dl_repr() {
-            let generator_U = RISTRETTO_BASEPOINT_POINT.clone();
-            let generator_V = RISTRETTO_BASEPOINT_POINT.clone() * 42;
+        let generator_u = RISTRETTO_BASEPOINT_POINT.clone();
+        let generator_v = RISTRETTO_BASEPOINT_POINT.clone() * Scalar::from(42 as u32);
 
-            let M = Vec::from([generator_U, generator_V]);
-            let mut x1 = Vec::from([Scalar::from(12345 as u32)]);
-            let mut x2 = Vec::from([Scalar::from(67891 as u32)]);
-            let witness = Vec::from([x1, x2]);
+        let matrix = Vec::from([generator_u, generator_v]);
+        let mut x1 = Scalar::from(12345 as u32);
+        let x2 = Scalar::from(67891 as u32);
+        let witness = Vec::from([x1, x2]);
 
-            let X = Vec::from([generatorU * x1 + generatorV * x2);
+        let statement = Vec::from([generator_u * x1 + generator_v * x2]);
 
-            let mut proof = prove(&M, &witness, &X).expect("generating proof should not fail");
+        let mut proof = prove(&matrix, &witness, &statement).expect("generating proof should not fail");
 
-            assert!(verify(&M,&X,&proof).is_ok());
+        println!("{:?}", proof);
+        println!("{:?}", verify(&matrix, &statement, &proof));
+        assert!(verify(&matrix, &statement, &proof).is_ok());
 
-            verify(&M,&X,&proof).expect("proof should be valid");
+        verify(&matrix, &statement, &proof).expect("proof should be valid");
 
-            proof[1] = proof[2];
+        proof[1] = proof[2];
 
-            assert!(verify(&M,&X,&proof).is_err());
+        assert!(verify(&matrix, &statement, &proof).is_err());
 
-            x1[0] = x1[0] + Scalar::from(1 as u32);
-            witness = Vec::from([x1, x2]);
-            assert!(prove(&M, &witness, &X).is_err()) // should not prove false statements
+        x1 = x1 + Scalar::from(1 as u32);
+        let bad_witness = Vec::from([x1, x2]);
+        assert!(prove(&matrix, &bad_witness, &statement).is_err()) // should not prove false statements
     }
-
 
     #[test]
     fn dl_pok() {
-            let M = Vec::from([RISTRETTO_BASEPOINT_POINT.clone()]);
-            let mut x = Vec::from([Scalar::from(12345 as u32)]);
-            let X = Vec::from([RistrettoPoint::mul_base(&x[0])]);
+        let matrix = Vec::from([RISTRETTO_BASEPOINT_POINT.clone()]);
+        let mut x = Vec::from([Scalar::from(12345 as u32)]);
+        let statement = Vec::from([RistrettoPoint::mul_base(&x[0])]);
 
-            let mut proof = prove(&M, &x, &X).expect("generating proof should not fail");
+        let mut proof = prove(&matrix, &x, &statement).expect("generating proof should not fail");
 
-            assert!(verify(&M,&X,&proof).is_ok());
+        assert!(verify(&matrix, &statement, &proof).is_ok());
 
-            verify(&M,&X,&proof).expect("proof should be valid");
+        verify(&matrix, &statement, &proof).expect("proof should be valid");
 
-            proof[0] = proof[1];
+        proof[0] = proof[1];
 
-            assert!(verify(&M,&X,&proof).is_err());
+        assert!(verify(&matrix, &statement, &proof).is_err());
 
-            x[0] = x[0] + Scalar::from(1 as u32);
-            assert!(prove(&M, &x, &X).is_err()) // should not prove false statements
-    }
-
-
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        x[0] = x[0] + Scalar::from(1 as u32);
+        assert!(prove(&matrix, &x, &statement).is_err()) // should not prove false statements
     }
 }
--- src/okamoto.rs
+++ src/okamoto.rs
@@ -11,34 +11,46 @@ pub fn prove(
     witness: &[Scalar],
     statement: &[RistrettoPoint],
 ) -> Result<Vec<Scalar>, ProveError> {
-    if matrix.len() != witness.len() * statement.len() {
+    let n_statement_dim = statement.len();
+    let m_witness_dim = witness.len();
+
+    // check that matrix is m times n
+    if matrix.len() != m_witness_dim * n_statement_dim {
         return Err(ProveError::InvalidDimensions);
     }
 
-    let M = witness.len();
-    let N = statement.len();
+    // recompute the statement from the matrix and witness so that we can check soundness
+    let recomputed_statement: Vec<RistrettoPoint> = (0..n_statement_dim)
+        .map(|i| {
+            (0..m_witness_dim)
+                .map(|j| matrix[i * n_statement_dim + j] * witness[j])
+                .sum::<RistrettoPoint>()
+        })
+        .collect();
 
-    let recomputed_statement: Vec<RistrettoPoint> = (0..N).map(|i| (0..M).map(|j| matrix[i*N+j] * witness[j]).sum::<RistrettoPoint>()).collect();
-
+    // don't prove false statements
     if recomputed_statement != statement {
         return Err(ProveError::Unsound);
     }
-    
-    let mut csprng = OsRng;
-    let commitment_trapdoors: Vec<Scalar> = (0..M).map(|_| Scalar::random(&mut csprng)).collect();
 
-    let commitments: Vec<RistrettoPoint> = (0..N)
+    let mut csprng = OsRng;
+    let commitment_trapdoors: Vec<Scalar> = (0..m_witness_dim).map(|_| Scalar::random(&mut csprng)).collect();
+
+    // compute R_i = \sum_{j=1}^m (r_j M_{i,j}) for all i in [n]
+    let commitments: Vec<RistrettoPoint> = (0..n_statement_dim)
         .map(|i| {
-            (0..M)
-                .map(|j| commitment_trapdoors[j] * matrix[i * N + j])
+            (0..m_witness_dim)
+                .map(|j| commitment_trapdoors[j] * matrix[i * n_statement_dim + j])
                 .sum()
         })
         .collect();
 
+    // fiat shamir: compute hash of matrix, statement, commitments
     let challenge = compute_challenge(matrix, statement, &commitments);
 
+    // proof is (c, s_1, ..., s_m)
     let mut proof = Vec::from([challenge]);
-    proof.extend((0..M).map(|i| (commitment_trapdoors[i] + challenge * witness[i])));
+    proof.extend((0..m_witness_dim).map(|i| (commitment_trapdoors[i] + challenge * witness[i])));
 
     Ok(proof)
 }
@@ -50,6 +62,7 @@ fn compute_challenge(
 ) -> Scalar {
     let mut hash = Sha512::new();
 
+    // double-and-compress because it is batchable and does not effect security
     for compressed_point in RistrettoPoint::double_and_compress_batch(&Vec::from(
         [matrix, statement, commitments].concat(),
     )) {
@@ -64,26 +77,26 @@ pub fn verify(
     statement: &[RistrettoPoint],
     proof: &[Scalar],
 ) -> Result<(), VerifyingError> {
-    let N = statement.len();
-    let M = proof.len() - 1;
+    let n_statement_dim = statement.len();
+    let m_witness_dim = proof.len() - 1;
 
     let challenge = proof[0];
     let responses = &proof[1..];
 
     // first step: recompute R_i's for all i in [m]
-    let recomputed_commitments: Vec<RistrettoPoint> = (0..N)
+    let recomputed_commitments: Vec<RistrettoPoint> = (0..n_statement_dim)
         .map(|i| {
-            (0..N)
-                .map(|j| responses[j] * matrix[i * N + j])
+            (0..m_witness_dim)
+                .map(|j| responses[j] * matrix[i * n_statement_dim + j])
                 .sum::<RistrettoPoint>()
                 - challenge * statement[i]
         })
         .collect();
 
-    let recomputed_challenge = compute_challenge(&matrix,&statement,&recomputed_commitments);
+    let recomputed_challenge = compute_challenge(&matrix, &statement, &recomputed_commitments);
 
-    if recomputed_challenge == proof[0] {
-        return Ok(()) 
+    if recomputed_challenge == challenge {
+        return Ok(());
     }
 
     Err(VerifyingError::Invalid)
